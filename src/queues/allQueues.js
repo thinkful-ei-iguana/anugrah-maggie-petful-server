0aa68e0d91ed115addd6a30067e42291dfe7a834
Anugrah Lambogo <lambogoai@gmail.com>
2020-01-31 11:45:37 -0600

    added count and placement functions

--- src/queues/allQueues.js
+++ src/queues/allQueues.js
@@ -18,10 +18,33 @@ function removePet() {
 function initializeHumans() {
   let humanQueue = new Queue();
   humanQueue.enqueue(humans[0].name);
-
 }
 
 function continueHumans() {
   let index = Math.floor(math.random() * humans.length);
   humanQueue.enqueue(humans[index].name);
+}
+
+function queueCount(queue) {
+  if (!queue.first) {
+    return console.log('nothing in the queue');
+  }
+
+  let count = 0;
+  let currNode = queue.first;
+  while(currNode) {
+    count++;
+    currNode = currNode.next;
+  }
+  return count;
+}
+
+function whereInLine(person, queue) {
+  let placement = 0;
+  let currNode = queue.first;
+  while(currNode.value !== person) {
+    placement++;
+    currNode = currNode.next;
+  }
+  return placement;
 }
