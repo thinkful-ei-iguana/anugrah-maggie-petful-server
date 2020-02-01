a42dedafcf8eac81efc5c4a55265ebf0919e91d9
therealmbittarelli <maggiekmcclellan@gmail.com>
2020-02-01 01:31:10 -0500

    testing line endings

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.get('/api/updateEvent', (req, res) => {
   res
     .writeHead(200, headers);
   setInterval(() => {
-    res.write(`data: hello world ${Date.now()}\n\n`);
+    res.write(`data: hello world ${Date.now()}\n`);
     // .write(JSON.stringify(humansRouter.getService().getQueue()));
     // res.end();
     res.flush();
